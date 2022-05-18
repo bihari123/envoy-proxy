7a60a282dec7a8e9c28d296a0f1110dc779fef64
bihari123 <thakur.cs.tarun@gmail.com>
2022-05-18 07:53:11 +0530

    some golang apps to test

---
+++ training-material/app2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Welcom to app 2!")
+	})
+
+	r.Run(":2222")
+}
